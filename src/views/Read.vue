ddf98af6ca2c5d3f8114b6cde8a351cf7f405697
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-07-13 11:41:41 +0700

    feat: upload and preview image

--- src/views/Read.vue
+++ src/views/Read.vue
@@ -4,9 +4,22 @@
     <div class="read-box">
       <div class="read-box-context">Hello, Hoang Tien Thinh</div>
       <div class="read-box-container">
-        <div class="image-box"></div>
+        <div class="image-box">
+          <img v-if="imageSrc" :src="imageSrc" alt="" />
+        </div>
         <div class="button-box">
-          <primary-button text="Upload" type="outlined" />
+          <input
+            type="file"
+            id="upload"
+            style="display: none"
+            @change="previewImage"
+            ref="uploadImage"
+          />
+          <primary-button
+            text="Upload"
+            type="outlined"
+            @click-button="handleUpload"
+          />
           <primary-button text="Convert" type="solid" />
         </div>
         <div class="result-box"></div>
@@ -16,11 +29,30 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 import HeaderMain from "../components/Header.vue";
 import PrimaryButton from "../components/PrimaryButton.vue";
 
+const uploadImage = ref(null);
+const image = ref({});
+const imageSrc = ref("");
+
 const logout = () => {
   console.log("Logout");
+};
+
+const handleUpload = () => {
+  console.log("UPLOAD");
+  uploadImage.value.click();
+  console.log(uploadImage.value);
+};
+
+const previewImage = () => {
+  image.value = uploadImage.value.files[0];
+  console.log(image.value);
+  const src = URL.createObjectURL(image.value);
+  imageSrc.value = src;
 };
 </script>
 
@@ -39,6 +71,13 @@ const logout = () => {
         height: 400px;
         border-radius: 8px;
         border: 4px solid #2c3639;
+      }
+      .image-box {
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
       }
       .button-box {
         display: flex;
